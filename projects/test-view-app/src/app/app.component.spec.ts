463523841b76a37d8a473868acec6a2189f391fc
Kirill Portnov <prodevkir@mail.ru>
2023-08-01 01:05:03 +0400

    up

--- projects/test-view-app/src/app/app.component.spec.ts
+++ projects/test-view-app/src/app/app.component.spec.ts
@@ -1,20 +1,20 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from '@angular/core/testing';
+import { AppComponent, TypeViewComponent } from './app.component';
 
 describe('AppComponent', () => {
-    let component: AppComponent;
-    let fixture: ComponentFixture<AppComponent>;
+    beforeEach(() => TestBed.configureTestingModule({
+        declarations: [AppComponent]
+    }));
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [AppComponent]
-        });
-        fixture = TestBed.createComponent(AppComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app).toBeTruthy();
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    it(`should have as title 'test'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.viewComponent).toEqual(TypeViewComponent.Picker);
     });
 });

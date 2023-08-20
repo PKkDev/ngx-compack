3f922d9e5c851ab6b3bb8c6f6f63f43877587323
Kirill <prodevkir@mail.ru>
2023-08-21 00:03:23 +0400

    update styles for button

--- projects/ngx-compack/src/lib/compack-button/cmp-button.directive.ts
+++ projects/ngx-compack/src/lib/compack-button/cmp-button.directive.ts
@@ -1,4 +1,4 @@
-import { ContentChild, Directive, ElementRef, Input, Renderer2, SimpleChanges } from '@angular/core';
+import { ContentChild, Directive, ElementRef, Input, OnChanges, AfterViewInit, Renderer2, SimpleChanges } from '@angular/core';
 import { CompackIconDirective } from '../compack-icon/compack-icon.directive';
 
 export type NgcButtonType = 'default' | 'primary' | 'dangerous' | 'mode';
@@ -6,7 +6,7 @@ export type NgcButtonType = 'default' | 'primary' | 'dangerous' | 'mode';
 @Directive({
   selector: '[cmp-button]'
 })
-export class CmpButtonDirective {
+export class CmpButtonDirective implements AfterViewInit, OnChanges {
 
   @Input() btnType: NgcButtonType = 'default';
   @Input() loading = false;
@@ -19,11 +19,13 @@ export class CmpButtonDirective {
       <path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"> </path>
     </svg>`;
 
+  private isAfterViewInit = false;
+
   constructor(private el: ElementRef, private renderer2: Renderer2) {
     this.renderer2.addClass(this.el.nativeElement, 'cmp-btn');
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     switch (this.btnType) {
       case 'primary':
         this.renderer2.addClass(this.el.nativeElement, 'cmp-btn-primary');
@@ -37,36 +39,45 @@ export class CmpButtonDirective {
       case 'default':
         break;
     }
+
+    this.checkChangeIcon();
+
+    this.isAfterViewInit = true;
   }
 
   ngOnChanges(changes: SimpleChanges) {
+    if (!this.isAfterViewInit) return;
+
     if (changes['loading']) {
+      this.checkChangeIcon();
+    }
+  }
 
-      if (this.loading) {
-        this.renderer2.setProperty(this.el.nativeElement, 'disabled', true);
+  private checkChangeIcon() {
+    if (this.loading) {
+      this.renderer2.setProperty(this.el.nativeElement, 'disabled', true);
 
-        if (this.ngcIcon) {
-          this.ngcIcon.setLoading();
-        } else {
-          if (!this.loadingSpan) {
-            const span = this.renderer2.createElement('span');
-            this.renderer2.addClass(span, 'cmp-span-icon');
-            this.renderer2.addClass(span, 'cmp-loading');
-            span.innerHTML = this.loadingSvg
-            this.loadingSpan = span;
-          }
-          this.renderer2.appendChild(this.el.nativeElement, this.loadingSpan);
+      if (this.ngcIcon) {
+        this.ngcIcon.setLoading();
+      } else {
+        if (!this.loadingSpan) {
+          const span = this.renderer2.createElement('span');
+          this.renderer2.addClass(span, 'cmp-span-icon');
+          this.renderer2.addClass(span, 'cmp-loading');
+          span.innerHTML = this.loadingSvg
+          this.loadingSpan = span;
         }
+        this.renderer2.appendChild(this.el.nativeElement, this.loadingSpan);
       }
-      else {
-        this.renderer2.setProperty(this.el.nativeElement, 'disabled', false);
+    }
+    else {
+      this.renderer2.setProperty(this.el.nativeElement, 'disabled', false);
 
-        if (this.ngcIcon) {
-          this.ngcIcon.unsetLoading();
-        } else {
-          if (this.loadingSpan)
-            this.renderer2.removeChild(this.el.nativeElement, this.loadingSpan);
-        }
+      if (this.ngcIcon) {
+        this.ngcIcon.unsetLoading();
+      } else {
+        if (this.loadingSpan)
+          this.renderer2.removeChild(this.el.nativeElement, this.loadingSpan);
       }
     }
   }
--- projects/ngx-compack/src/lib/compack-icon/compack-icon.directive.ts
+++ projects/ngx-compack/src/lib/compack-icon/compack-icon.directive.ts
@@ -15,10 +15,10 @@ export class CompackIconDirective implements OnInit, AfterViewInit, OnChanges {
       d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z">
     </path>`;
 
-  @ContentChild('svg') svg: ElementRef | undefined;
+  @ContentChild('svg') svg: SVGSVGElement | null = null;
 
   constructor(private el: ElementRef, private renderer2: Renderer2) {
-    this.renderer2.addClass(this.el.nativeElement, 'span-icon');
+    this.renderer2.addClass(this.el.nativeElement, 'cmp-span-icon');
   }
 
   ngOnInit() {
@@ -28,9 +28,12 @@ export class CompackIconDirective implements OnInit, AfterViewInit, OnChanges {
   }
 
   ngAfterViewInit() {
+    this.svg = (this.el.nativeElement as HTMLElement).querySelector('svg');
+
     if (this.loading) {
       this.setLoading();
     }
+
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -44,18 +47,18 @@ export class CompackIconDirective implements OnInit, AfterViewInit, OnChanges {
 
   public setLoading() {
     if (this.svg) {
-      this.renderer2.addClass(this.el.nativeElement, 'ngc-loading');
-      const svgEl = (this.svg.nativeElement as SVGElement);
-      this.originSvgPath = svgEl.innerHTML;
-      svgEl.innerHTML = this.loadingSvgPath;
+      this.renderer2.addClass(this.el.nativeElement, 'cmp-loading');
+      this.originSvgPath = this.svg.innerHTML;
+      this.svg.innerHTML = this.loadingSvgPath;
     }
   }
 
   public unsetLoading() {
     if (this.svg) {
-      this.renderer2.removeClass(this.el.nativeElement, 'ngc-loading');
-      const svgEl = (this.svg.nativeElement as SVGElement);
-      svgEl.innerHTML = this.originSvgPath!;
+      this.renderer2.removeClass(this.el.nativeElement, 'cmp-loading');
+      if (this.originSvgPath) {
+        this.svg.innerHTML = this.originSvgPath;
+      }
     }
   }
 
